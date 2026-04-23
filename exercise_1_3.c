afef035cbf813d4a426804fb77ecf64e4446fe21
manly-toucan <nerdspective0@gmail.com>
2026-04-23 14:59:21 +0530

    first exercise added

---
+++ exercise_1_3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+	
+	// => This is a comment in C
+	/* => This is a comment in C */
+
+
+	//  variable declaration 
+	float fahr, celsius;
+	int lower_limit, upper_limit, step_size;
+
+	// variable initialization 
+	lower_limit = 0;	// lower limit of temperature table
+	upper_limit = 300;	// upper limit of temperature table 
+	step_size = 20;		// step size
+	
+	// set fahr equal to lower_limit of temperature table 
+	fahr = lower_limit;
+
+	// printing table column heading 
+	printf("Fahrenheit -> Celsius\n");
+
+	// running while loop to print reults to the terminal 
+	while (fahr <= upper_limit) {
+
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%.1f°F -> %.1f°C\n", fahr, celsius);
+
+		// fahr will increase by step_size i.e, 20
+		fahr = fahr + step_size;
+	}
+
+}
