6e2b5312249c5be8c8adf2eb9621365342c999ba
Ayananta Paul <nerdspective0@gmail.com>
2026-05-02 16:55:15 +0530

    added exercise 1.1

---
+++ chapter_1/exercise_1_1.c
@@ -0,0 +1,22 @@
+/* -- Exercise 1.1 --
+ * Run the "Hello, World!" program on your system. 
+ * Experiment with leaving out parts of the program, to see what error 
+ * messages you get. 
+ * */
+
+// ==> This is a comment in C
+/* ==> This is a comment in C */ 
+#include <stdio.h>
+
+int main(void) {
+	
+	// print hello world to the terminal
+	printf("Hello, World!\n");
+
+	return 0;
+}
+
+/* a basic hello world program is given to you 
+ * your task is to run the program by commenting out 
+ * parts of the program and see if you get any errors 
+ */
